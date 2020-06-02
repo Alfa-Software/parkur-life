6e54002e29a225fdb2844d1d35bba50e9f402df1
JN513 <jn5.guape@gmail.com>
2020-06-02 09:28:02 -0300

    testando personagem

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -9,16 +9,21 @@ var motion = Vector2()#vector2 e pra agregar dois valores em uma variavel
 func _physics_process(_delta):#movimentos
 	
 	motion.y += GRAVITY
+	$Sprite.play("down")
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x=SPEED
+		$Sprite.play("walk")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x=-SPEED
+		$Sprite.play("walk")
 	else:
 		motion.x=0
+		$Sprite.play("stop")
 		
 	if is_on_floor():#pulo
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+			$Sprite.play("jump")
 		
 	motion = move_and_slide(motion, UP)
